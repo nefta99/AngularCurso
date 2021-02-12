a1fb40398ae7d27c850133b0f4ff763e61f5b2de
nef <neftalitorres@itesm.mx>
2021-02-11 21:50:59 -0600

    se termina el videl #21

--- MiPrimeraAppAngular/MiPrimeraAppAngular/ClientApp/src/app/app.module.ts
+++ MiPrimeraAppAngular/MiPrimeraAppAngular/ClientApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -67,6 +68,7 @@ import { PersonaFormMantenimientoComponent } from './components/persona-form-man
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: 'filtradoProductoCategoria', component: FiltradoProductoCategoriaComponent, pathMatch: 'full' },
       { path: 'filtradoProductoNombre', component: FiltradoProductoNombreComponent },
--- MiPrimeraAppAngular/MiPrimeraAppAngular/ClientApp/src/app/components/persona-form-mantenimiento/persona-form-mantenimiento.component.ts
+++ MiPrimeraAppAngular/MiPrimeraAppAngular/ClientApp/src/app/components/persona-form-mantenimiento/persona-form-mantenimiento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-persona-form-mantenimiento',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonaFormMantenimientoComponent implements OnInit {
 
-  constructor() { }
+  persona: FormGroup;
+  constructor() {
+    this.persona = new FormGroup(
+      {
+        'iidpersona': new FormControl("0"),
+        'nombre': new FormControl("", [Validators.required, Validators.maxLength(100)]),
+        'apPaterno': new FormControl("", [Validators.required, Validators.maxLength(150)]),
+        'apMaterno': new FormControl("", [Validators.required, Validators.maxLength(150)]),
+        'telefono': new FormControl("", [Validators.required, Validators.maxLength(10)]),
+        'correo': new FormControl("", [Validators.required, Validators.maxLength(150)])
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+  guardarDatos() {
+
   }
 
 }
